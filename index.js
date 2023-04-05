f0805a3336e9f58ab983bf594e174dff53462d11
samirmaikap <maikap.samir@gmail.com>
2023-04-05 19:59:46 +0530

    Rename app

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Platform} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(
+    Platform.OS === 'android' ? appName : 'biggestask',
+    () => App,
+);
